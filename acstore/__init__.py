bbb354359d8ec1b2d9d0d8ca8a3cebabe75dc94b
Joachim Metz <joachim.metz@gmail.com>
2017-10-13 17:49:55 +0200

    Added project skeleton

---
+++ acstore/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Attribute Container Storage (ACStore).
+
+ACStore, or Attribute Container Storage, provides a stand-alone implementation
+to read and write plaso storage files.
+"""
+
+__version__ = '20171013'
